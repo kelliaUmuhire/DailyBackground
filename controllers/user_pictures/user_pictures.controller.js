5d53a3ded28c9d481dbd08002122407bb7379a82
izabayo7 <cedricizabayo7@gmail.com>
2021-03-29 13:50:52 +0200

    deleting element if records exceed 7

--- controllers/user_pictures/user_pictures.controller.js
+++ controllers/user_pictures/user_pictures.controller.js
@@ -6,7 +6,7 @@ const { UserPicture } = require("../../models/UserPictures.model");
 dotenv.config();
 
 const unsplash = new createApi({
-  accessKey: process.env.ACCESS_KEY,
+    accessKey: process.env.ACCESS_KEY,
 });
 
 /**
@@ -15,12 +15,12 @@ const unsplash = new createApi({
  * @param res
  */
 exports.getUserLatestImage = async (req, res) => {
-  try {
-    let user_pic = await UserPicture.findOne({ user: req.user._id });
-    res.status(200).send({ user_pic: user_pic });
-  } catch (err) {
-    return res.send(formatResult(500, err));
-  }
+    try {
+        let user_pic = await UserPicture.findOne({ user: req.user._id });
+        res.status(200).send({ user_pic: user_pic });
+    } catch (err) {
+        return res.send(formatResult(500, err));
+    }
 };
 
 /**
@@ -29,36 +29,37 @@ exports.getUserLatestImage = async (req, res) => {
  * @param res
  */
 exports.LoadNewImage = async function (req, res) {
-  try {
-    const { data } = await unsplash.photos.getRandom({});
-    let user_pic = await UserPicture.findOne({ user: req.user._id });
-    const today = new Date().toISOString().split("T")[0];
-    const new_pic = {
-      date: today,
-      regular: data.urls.regular,
-      small: data.urls.small,
-      thumb: data.urls.thumb,
-    };
+    try {
+        const { data } = await unsplash.photos.getRandom({});
+        let user_pic = await UserPicture.findOne({ user: req.user._id });
+        const today = new Date().toISOString().split("T")[0];
+        const new_pic = {
+            date: today,
+            regular: data.urls.regular,
+            small: data.urls.small,
+            thumb: data.urls.thumb,
+        };
 
-    if (!user_pic) {
-      user_pic = new UserPicture({
-        user: req.user._id,
-        last_pics: [new_pic],
-      });
-    } else {
-      const isThisDayFound = user_pic.last_pics.filter((e) => e.data == today);
-      if (!isThisDayFound.length) {
-        if (user_pic.last_pics.length < 7) {
-          user_pic.last_pics.push(new_pic);
+        if (!user_pic) {
+            user_pic = new UserPicture({
+                user: req.user._id,
+                last_pics: [new_pic],
+            });
+        } else {
+            const isThisDayFound = user_pic.last_pics.filter((e) => e.data == today);
+            if (!isThisDayFound.length) {
+                if (user_pic.last_pics.length == 7)
+                    user_pic.last_pics.shift();
+
+                user_pic.last_pics.push(new_pic);
+            } else {
+                const index = user_pic.last_pics.indexOf(isThisDayFound[0]);
+                user_pic.last_pics[index] = new_pic;
+            }
         }
-      } else {
-        const index = user_pic.last_pics.indexOf(isThisDayFound[0]);
-        user_pic.last_pics[index] = new_pic;
-      }
+        await user_pic.save();
+        res.status(200).send({ urls: user_pic });
+    } catch (err) {
+        return res.send(formatResult(500, err));
     }
-    await user_pic.save();
-    res.status(200).send({ urls: user_pic });
-  } catch (err) {
-    return res.send(formatResult(500, err));
-  }
 };
