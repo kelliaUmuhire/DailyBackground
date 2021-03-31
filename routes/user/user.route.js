1745ccb7cadff3f0a3984cdc75e92406af86b72f
kelliaUmuhire <kellumuhire@gmail.com>
2021-03-31 16:28:19 +0200

    load latest

--- routes/user/user.route.js
+++ routes/user/user.route.js
@@ -1,32 +1,40 @@
-const express = require('express');
-const PictureController = require('../../controllers/picture.contoller');
-const { createUser, updateUser, deleteUser, getUserById, checkEmailExistance, checkUsernameExistence, userLogin, updateUserPassword } = require('../../controllers/user/user.controller');
-const { LoadNewImage } = require('../../controllers/user_pictures/user_pictures.controller');
-const { auth } = require('../../middlewares/auth.middleware');
+const express = require("express");
+const PictureController = require("../../controllers/picture.contoller");
+const {
+  createUser,
+  updateUser,
+  deleteUser,
+  getUserById,
+  checkEmailExistance,
+  checkUsernameExistence,
+  userLogin,
+  updateUserPassword,
+} = require("../../controllers/user/user.controller");
+const {
+  LoadNewImage,
+  getUserLatestImage,
+} = require("../../controllers/user_pictures/user_pictures.controller");
+const { auth } = require("../../middlewares/auth.middleware");
 const router = express.Router();
 
-router.route('/')
-    .post(createUser)
-    .put([auth, updateUser])
-    .delete([auth, deleteUser])
+router
+  .route("/")
+  .post(createUser)
+  .put([auth, updateUser])
+  .delete([auth, deleteUser]);
 
-router.route('/load_new_image')
-    .get([auth, LoadNewImage])
+router.route("/load_new_image").get([auth, LoadNewImage]);
 
-router.route('/:id')
-    .get(getUserById)
+router.route("/latest").get([auth, getUserLatestImage]);
 
-router.route('/checkEmailExistance/:email')
-    .get(checkEmailExistance)
+router.route("/:id").get(getUserById);
 
-router.route('/checkUsernameExistance/:user_name')
-    .get(checkUsernameExistence)
+router.route("/checkEmailExistance/:email").get(checkEmailExistance);
 
+router.route("/checkUsernameExistance/:user_name").get(checkUsernameExistence);
 
-router.route('/login')
-    .post(userLogin)
+router.route("/login").post(userLogin);
 
-router.route('/updatePassword')
-    .put([auth, updateUserPassword])
+router.route("/updatePassword").put([auth, updateUserPassword]);
 
-exports.UserRoutes = router
+exports.UserRoutes = router;
